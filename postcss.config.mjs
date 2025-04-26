f191f7ada66939ffd9be4a21295c897d2dd6169b
gehirudm <gehirudm@pm.me>
2025-04-26 18:54:35 +0530

    configure mantine ui

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,7 @@
-import mantinePreset from 'postcss-preset-mantine';
-import simpleVars from 'postcss-simple-vars';
-
 const config = {
-  plugins: [
-    "@tailwindcss/postcss",
-    mantinePreset(),
-    simpleVars({
+  plugins: {
+    'postcss-preset-mantine': {},
+    'postcss-simple-vars': {
       variables: {
         'mantine-breakpoint-xs': '36em',
         'mantine-breakpoint-sm': '48em',
@@ -13,8 +9,9 @@ const config = {
         'mantine-breakpoint-lg': '75em',
         'mantine-breakpoint-xl': '88em',
       },
-    }),
-  ],
+    },
+    "@tailwindcss/postcss": {},
+  },
 };
 
 export default config;
